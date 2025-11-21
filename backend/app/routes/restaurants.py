2cf7446dc2d55da76777a971fce4e49a33f37d60
Ivan Torres <itorr4@uic.edu>
2025-11-20 18:04:45 -0600

    snapshot for Adam

--- backend/app/routes/restaurants.py
+++ backend/app/routes/restaurants.py
@@ -5,13 +5,32 @@ restaurants_bp = Blueprint("restaurants", __name__)
 import psycopg2
 import sys
 
-params = {
+db_params = {
             "host": "localhost",
             "database": "uni_eats",
             "user": "uni_eats_user",
             "password": "password",
             "port": 5432 # Default port is 5432
         }
+
+# I believe params has to be a tuple
+def db_retrieve(query, params=[]):
+    try:
+        conn = psycopg2.connect(**db_params)
+        cursor = conn.cursor()
+        
+        # params would be used for parameterized queries
+        cursor.execute(query, *params)
+        res = cursor.fetchall()
+        
+        cursor.close()
+        conn.close()
+        
+        return res
+    except Exception as e:
+        print("Unable to retrieve content from the db.")
+        print(e)
+        sys.exit(1)
 
 @restaurants_bp.get("/")
 def list_restaurants():
@@ -24,4 +43,5 @@ def list_restaurants():
       - return JSON for the React UI
     """
     # make connection to database, cursor, execute query
+    print(db_retrieve("Select VERSION();"))
     return jsonify({"message": "TODO: implement restaurant listing API"})
--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
-from app import create_app
+from flask import Flask
+from app.routes.restaurants import restaurants_bp
 
-app = create_app()
+app = Flask(__name__)
+app.register_blueprint(restaurants_bp, url_prefix="/restaurants")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
